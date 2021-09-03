d6cbff69fc10180672c56bf977564b7c1c305ad1
Marcel Piva <marcelpiva@Marcels-MacBook-Pro.local>
2021-09-03 17:23:04 -0300

    Tests Failure

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,15 +10,15 @@ public class DateUtilsTest {
 	@Test
 	public void shouldReturnTrueWithFutureDates() {
 		LocalDate date = LocalDate.of(2030, 01, 01);
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
 	}
-	
+
 	@Test
 	public void shouldReturnTrueWithPresentDate() {
 		LocalDate date = LocalDate.now();
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
 	}
-	
+
 	@Test
 	public void shouldReturnFalseWithPastDates() {
 		LocalDate date = LocalDate.of(2012, 01, 01);
